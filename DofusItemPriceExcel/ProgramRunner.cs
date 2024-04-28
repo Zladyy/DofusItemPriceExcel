9b9560a1f15205803f4c2188b323ef0cf72bf90d
Maxime TOMBEUX <maxime.tombeux@gmail.com>
2024-04-28 17:21:19 +0200

    cleanup

--- DofusItemPriceExcel/ProgramRunner.cs
+++ DofusItemPriceExcel/ProgramRunner.cs
@@ -6,9 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DofusItemPriceExcelPj
 {
